51d0e12f15dcde13957db4e06eda26fe72da9405
Hubersito <araujohuber1234@gmail.com>
2025-06-10 10:08:05 -0500

    10-06-2025

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+const BD_HOST = 'localhost';
+const BD_NAME = 'inventario';
+const BD_USER = 'root';
+const BD_PASSWORD = '';
+const BD_CHARSET = 'utf8';
+
+//ingresar link de proyecto
+const BASE_URL = 'http://localhost:8888/dpweb/';
+
+?>
