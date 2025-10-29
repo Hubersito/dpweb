f240df2aa5fc52e51f48f0b9c90b570000e75807
Hubersito <araujohuber1234@gmail.com>
2025-10-29 12:12:11 -0500

    1

---
+++ view/proveedor-new.php
@@ -0,0 +1,90 @@
+
+
+    <!-- inicio de cuerpo de pagina-->
+    <div class="container" style="margin-top: 100px;">
+        <div class="card">
+            <div class="card-header" style="text-align:center;">
+                Registro de Proveedor
+            </div>
+            <form id="frm_proveedor" action="" method="">
+                <div class="card-body">
+
+                    <div class="mb-3 row">
+                        <label for="nro_identidad" class="col-sm-2 col-form-label">Nro de Documento</label>
+                        <div class="col-sm-10">
+                            <input type="number" class="form-control" id="nro_identidad" name="nro_identidad" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="razon_social" class="col-sm-2 col-form-label">Nombre/Razon Social</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="razon_social" name="razon_social" required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="telefono" class="col-sm-2 col-form-label">Telefono</label>
+                        <div class="col-sm-10">
+                            <input type="number" class="form-control" id="telefono" name="telefono"required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="correo" class="col-sm-2 col-form-label">Correo</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" id="correo" name="correo"required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="departamento" class="col-sm-2 col-form-label">Departamento</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="departamento" name="departamento"required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="provincia" class="col-sm-2 col-form-label">Provincia</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="provincia" name="provincia"required>
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="distrito" class="col-sm-2 col-form-label">Distrito</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="distrito" name="distrito"required>
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="cod_postal" class="col-sm-2 col-form-label">Codigo Postal</label>
+                        <div class="col-sm-10">
+                            <input type="number" class="form-control" id="cod_postal" name="cod_postal"required>
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="direccion" class="col-sm-2 col-form-label">Direccion</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="direccion" name="direccion"required>
+                        </div>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="rol" class="col-sm-2 col-form-label">Rol</label>
+                        <div class="col-sm-10">
+                            <select class="form-select" aria-label="Default select example" id="rol" name="rol" required readonly>
+                                <option value="proveedor" selected>Proveedor</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div style="display: flex; justify-content:center; gap:20px">
+                        <button type="submit" class="btn btn-primary">Registrar</button>
+                        <button type="reset" class="btn btn-info">Limpiar</button>
+                        <button type="button" class="btn btn-danger">Cancelar</button>
+                        <a href="<?php echo BASE_URL; ?>proveedor-list" class="btn btn-success">ver</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!--fin de cuerpo de pagina-->
+    <script src="<?php echo BASE_URL; ?>view/function/proveedor.js"></script>
+    
---
+++ view/users.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <h5 class="mt-3 text-center" style="background: blue; align-items:center;">LISTA DE USUARIOS</h5>
+    <table class="table table-striped text-center" style="border: 2px solid blue;">
+        <thead>
+            <tr>
+                <th>Nro</th>
+                <th>DNI</th>
+                <th>Nombres y Apellidos</th>
+                <th>Correo</th>
+                <th>Rol</th>
+                <th>Estado</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody id="content_users">
+
+
+        </tbody>
+    </table>
+</div>
+<script src="<?= BASE_URL ?>view/function/user.js"></script>
+<!--<script>view_users();</script>-->
